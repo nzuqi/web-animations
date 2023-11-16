9c2c178f660c352bcafd236f8aa64c7628b2e9b2
Martin Nzuki <iamnzuki@gmail.com>
2023-11-16 10:27:28 +0300

    Added notifications badge animation

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,19 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  animations: [
+    trigger('animateNotificationsBadge', [
+      transition(':enter', [
+        style({ transform: 'scale(0)' }),
+        animate('200ms 1s ease-in', style({ transform: 'scale(1.8)' })),
+        animate('200ms ease-out', style({ transform: 'scale(1)' })),
+      ])
+    ])
+  ]
 })
 export class DashboardComponent implements OnInit {
 
