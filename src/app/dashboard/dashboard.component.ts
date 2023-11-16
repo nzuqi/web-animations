fffe682a330cc6910e26d9c84abfb37ab057372a
Martin Nzuki <iamnzuki@gmail.com>
2023-11-16 11:41:11 +0300

    Animate cards on dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { animate, style, transition, trigger } from '@angular/animations';
+import { animate, query, sequence, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -12,7 +12,19 @@ import { Component, OnInit } from '@angular/core';
         animate('200ms 1s ease-in', style({ transform: 'scale(1.8)' })),
         animate('200ms ease-out', style({ transform: 'scale(1)' })),
       ])
-    ])
+    ]),
+    trigger('animateCards', [
+      transition(':enter', [
+        query('.cards .card', style({ opacity: 0, marginTop: '100px' })),
+        sequence([
+          query('.card:nth-child(1)', animate('400ms ease-in', style({ opacity: 1, marginTop: '0px' }))),
+          query('.card:nth-child(2)', animate('400ms ease-in', style({ opacity: 1, marginTop: '0px' }))),
+          query('.card:nth-child(3)', animate('400ms ease-in', style({ opacity: 1, marginTop: '0px' }))),
+          query('.card:nth-child(4)', animate('400ms ease-in', style({ opacity: 1, marginTop: '0px' }))),
+          query('.card:nth-child(5)', animate('400ms ease-in', style({ opacity: 1, marginTop: '0px' }))),
+        ])
+      ])
+    ]),
   ]
 })
 export class DashboardComponent implements OnInit {
